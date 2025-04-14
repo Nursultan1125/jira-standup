3b620632a1800b1783fcdee588ea5dc831e8d04b
Nursultan Joldoshbekov <nursultan@maddevs.io>
2025-04-14 12:25:17 +0600

    Remove useless code

--- main.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("2.json", "r") as file:
-    data = json.load(file)
-    print(data["issues"][0]["fields"]["worklog"]["worklogs"][-1]["created"])
-    print(len(data["issues"][3]["fields"]["worklog"]["worklogs"]))
